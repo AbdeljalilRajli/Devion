735a729cef1596697d69068e31980df7325e4fe8
Abdeljalil Rajli <rajli.contact@gmail.com>
2024-09-22 23:38:31 +0100

    approach pt1

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
 import Grid from "@/components/Grid";
@@ -16,6 +17,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
