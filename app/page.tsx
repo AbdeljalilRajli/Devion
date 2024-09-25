1730d6e3615788e24124ea952647f91e18861b9f
Abdeljalil Rajli <rajli.contact@gmail.com>
2024-09-25 22:25:48 +0100

    footer pt1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import HeroSection from "@/components/HeroSecion";
 import RecentProjects from "@/components/RecentProjects";
@@ -18,6 +19,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
