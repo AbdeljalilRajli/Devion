1c44de655cbe06ce0f6d7f1a372b763fb9e49cea
AbdeljalilRajli <rajli.contact@gmail.com>
2024-10-03 13:01:27 +0100

    Logo and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./provider";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Devion",
   description: "Next generation modern agency",
+  icons: {
+    icon: "/favicon.png", // Path to your favicon
+  },
 };
 
 export default function RootLayout({
@@ -18,15 +20,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* The metadata will automatically add the title, description, and favicon */}
+        <link rel="icon" href="/favicon.png" sizes="any" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.png" />
+        <meta name="description" content="Next generation modern agency" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Devion</title>
+      </head>
       <body className={inter.className}>
-      <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-      </ThemeProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,11 +35,15 @@ const Footer = () => {
           Copyright © 2024 Devion.
         </p>
 
+        <div className="flex items-center">
+          <img src="/devion-Logo-white.png" alt="footer-logo" />
+        </div>
+
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-95 bg-indigo-900 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
             </div>
